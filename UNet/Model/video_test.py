fb4b622bcec4c05ae26746e2d4942a6f9aaff716
hizircanbayram <hizircanbayram@gmail.com>
2020-06-22 17:13:20 +0300

    bounding box marked

--- UNet/Model/video_test.py
+++ UNet/Model/video_test.py
@@ -10,9 +10,19 @@ from unet20 import unet
 from keras.preprocessing.image import load_img
 from keras.preprocessing.image import img_to_array
 
+
+def getBB(points):
+    points = np.transpose(np.array(points))
+    x1 = min(points[0])
+    y1 = min(points[1])
+    x2 = max(points[0])
+    y2 = max(points[1])
+    return (x1, y1), (x2, y2)
+
+
 cap = cv2.VideoCapture('HIJEL(32).mp4')
 
-model_path = 'UnetDataset_v4/unet20.hdf5'
+model_path = 'check_points/unet20_52_256/Unet20_07-0.96.hdf5'
 dependencies = {
     'iou_loss_core': iou_loss_core
 }
@@ -24,18 +34,26 @@ while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    frame_resized = cv2.resize(frame, (512, 512), interpolation=cv2.INTER_LINEAR)
-    resized = cv2.resize(frame_rgb, (512, 512), interpolation=cv2.INTER_LINEAR)
-    #resized = img_to_array(load_img(dir_name + ID, color_mode='rgb', target_size=(512,512,3), interpolation='bilinear')).astype(int)
-    mask = get_mask(model, resized)
-    #img_1_1 = cv2.cvtColor(resized, cv2.cv2.COLOR_BGR2RGB)
-    hand_img = getHandArea(frame_resized, mask)    
+    frame_resized = cv2.resize(frame, (256, 256), interpolation=cv2.INTER_LINEAR)
+    resized = cv2.resize(frame_rgb, (256, 256), interpolation=cv2.INTER_LINEAR)
+    mask = get_mask(model, resized, 0)
+    contours, hierarchy = cv2.findContours(mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    hand_img = getHandArea(frame_resized, mask)   
+    #for c in contours:
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)
+    rect = cv2.minAreaRect(contours[0])
+    indices = []
+    box = cv2.boxPoints(rect)
+    box = np.int0(box)
+    p1, p2 = getBB(box)
+    cv2.rectangle(frame_resized, p1, p2, (255,0,0), 2)
+
     # Display the resulting frame
-    cv2.imshow('frame',hand_img)
+    cv2.imshow('frame',frame_resized)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
+  
 # When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
-
