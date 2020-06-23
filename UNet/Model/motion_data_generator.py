b078cf7aeb88acf0b28a09d9fcf3f80ef08dbf8a
hizircanbayram <hizircanbayram@gmail.com>
2020-06-23 09:24:52 +0300

    motion_data_generator updated based on the new optical flow dataset

--- UNet/Model/motion_data_generator.py
+++ UNet/Model/motion_data_generator.py
@@ -4,7 +4,7 @@ import cv2
 from keras.preprocessing.image import load_img
 from keras.preprocessing.image import img_to_array
 from keras.preprocessing.image import array_to_img
-import matplotlib.pyplot as plt
+
 
 class MotionDataGenerator(keras.utils.Sequence):
     'Generates data for Keras'
@@ -83,15 +83,11 @@ class MotionDataGenerator(keras.utils.Sequence):
             x_inp = np.zeros(self.dim)
             sample = img_to_array(load_img(dir_name + ID, color_mode='rgb', interpolation='bilinear', target_size=(self.dim[0], self.dim[1], 3)), dtype='uint8')
             # optical flow
-            sample_gray = img_to_array(load_img(dir_name + ID, color_mode='grayscale', interpolation='bilinear', target_size=(self.dim[0], self.dim[1], 3)), dtype='uint8')
-            sample_gray_next = img_to_array(load_img(self.getNextNthFrameName(self.optical_flow_dir + ID), color_mode='grayscale', interpolation='bilinear', target_size=(self.dim[0], self.dim[1], 3)), dtype='uint8')
-            flow = cv2.calcOpticalFlowFarneback(sample_gray,sample_gray_next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
-            mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
-            ang = ang*180/np.pi/2 # need to be normalized, just like normal rgb hand images. it will be normalized below.
-            mag = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX) # need to be normalized, just like normal rgb hand images. it will be normalized below.
-            x_inp[:,:,0:3] = sample / 255.
-            x_inp[:,:,3] = ang / 180.
-            x_inp[:,:,4] = mag / 255.
+            optical_flow_img = cv2.imread('motion_vectors_' + str(self.nth_frame_for_motion) + '/' + ID)
+            optical_flow_img_hsv = cv2.cvtColor(optical_flow_img, cv2.COLOR_BGR2HSV)       
+            x_inp[:,:,0:3] = sample / 255.  
+            x_inp[:,:,3] = optical_flow_img_hsv[:,:,0] / 180.
+            x_inp[:,:,3] = optical_flow_img_hsv[:,:,2] / 255.
             X.append(x_inp)
             '''
             # test for normalized version of motion channels' distribution
@@ -100,17 +96,12 @@ class MotionDataGenerator(keras.utils.Sequence):
             cv2.waitKey(0)
             # test for normalized version of motion channels' distribution
             '''
-            '''
+            
             # test for motion images
-            hsv = np.zeros_like(sample)
-            hsv[...,1] = 255
-            hsv[...,0] = ang / 180.
-            hsv[...,2] = mag / 255.
-            rgb = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
-            cv2.imshow('motion time 8)', rgb)
-            cv2.waitKey(0)
+            #cv2.imshow('motion time 8)', optical_flow_img)
+            #cv2.waitKey(0)
             # test for motion images
-            '''
+            
             # optical flow
             #print('X.shape: ', sample.shape)
             if sample is None:
@@ -132,4 +123,3 @@ class MotionDataGenerator(keras.utils.Sequence):
         y = np.array(y)
         y = y[:, :, :, np.newaxis]
         return X, y
-
