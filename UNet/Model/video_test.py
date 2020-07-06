bc1a37738fb1eab27594bcc803b371ff88040e37
hizircanbayram <hizircanbayram@gmail.com>
2020-07-06 06:26:55 +0300

    BB algorithm changed(dne based on extreme points)

--- UNet/Model/video_test.py
+++ UNet/Model/video_test.py
@@ -3,7 +3,7 @@ import cv2
 
 from keras.models import load_model
 
-from measure_jaccard import query_img_names, get_mask, iou_loss_core
+from measure_jaccard import query_img_names, get_mask, iou_loss_score
 from predict import getHandArea
 from unet20 import unet
 
@@ -21,69 +21,92 @@ def getBB(points):
     return (x1, y1), (x2, y2)
 
 
+
+def getExtremeImg(im2, cnt):
+    leftmost = tuple(cnt[cnt[:,:,0].argmin()][0])
+    rightmost = tuple(cnt[cnt[:,:,0].argmax()][0])
+    topmost = tuple(cnt[cnt[:,:,1].argmin()][0])
+    bottommost = tuple(cnt[cnt[:,:,1].argmax()][0])
+
+    #--- drawing these points on the image ----
+    cv2.circle(im2, leftmost, 6, (0, 0, 255), -1)
+    cv2.circle(im2, rightmost, 6, (0, 0, 255), -1)
+    cv2.circle(im2, topmost, 6, (0, 255, 0), -1)
+    cv2.circle(im2, bottommost, 6, (0, 255, 0), -1)
+
+    return leftmost, rightmost, topmost, bottommost
+
+
+
+def makeBlackBoundriesOfImage(mask):
+    mask[:,len(mask[0,:,0])-1,0] = 0
+    mask[len(mask[0,:,0])-1,:,0] = 0
+    mask[0,:,0] = 0
+    mask[:,0,0] = 0
+
+
+
+def addPOintsToTheList(lst, lm, rm, tm, bm):
+    lst.append(lm)
+    lst.append(rm)
+    lst.append(tm)
+    lst.append(bm)
+    return lst
+
+
 cap = cv2.VideoCapture('HIJEL(32).mp4')
 
-model_path = 'check_points/unet20_52_256/Unet20_07-0.96.hdf5'
+
 dependencies = {
-    'iou_loss_core': iou_loss_core
+    'iou_loss_score': iou_loss_score
 }
-model = load_model(model_path, custom_objects=dependencies)
-#model = unet()
-#print(model.summary())
-my_key = False
+model = load_model('check_points/unet20_50_512/5_temmuz_unet20_013_50.hdf5', custom_objects=dependencies)
+
+
+
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    frame_resized = cv2.resize(frame, (256, 256), interpolation=cv2.INTER_LINEAR)
-    resized = cv2.resize(frame_rgb, (256, 256), interpolation=cv2.INTER_LINEAR)
+    frame_bgr = cv2.resize(frame, (512, 512), interpolation=cv2.INTER_LINEAR)
+    resized = cv2.resize(frame_rgb, (512, 512), interpolation=cv2.INTER_LINEAR)
+
     mask = get_mask(model, resized, 0)
-    contours, hierarchy = cv2.findContours(mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-    hand_img = getHandArea(frame_resized, mask)   
-    #for c in contours:
+    makeBlackBoundriesOfImage(mask)
+
+    contours, hierarchy = cv2.findContours(mask,cv2.RETR_CCOMP,cv2.CHAIN_APPROX_SIMPLE)
     contours = sorted(contours, key=cv2.contourArea, reverse=True)
-    #indices = []
-    rect1 = cv2.minAreaRect(contours[0])
-    box1 = cv2.boxPoints(rect1)
-    box1 = np.int0(box1)
-    p3_1, p3_2 = getBB(box1)
-    #cv2.rectangle(frame_resized, p3_1, p3_2, (0,0,255), 2)   
-    indices = None
+
+    indices = []
+    box1_area = None
+    box2_area = None
+
+    if len(contours) >= 1:
+        rect1 = cv2.minAreaRect(contours[0])
+        lm, rm, tm, bm = getExtremeImg(frame_bgr, contours[0])
+        indices1 = [] # this is created to calculate the area of the first contour, which is the biggest area
+        indices1 = addPOintsToTheList(indices1, lm, rm, tm, bm)
+        p1_1, p1_2 = getBB(indices1)
+        box1_area = (p1_2[0] - p1_1[0]) * (p1_2[1] - p1_1[1])
+        if box1_area > 1000:
+            indices = addPOintsToTheList(indices, lm, rm, tm, bm) # adding points to the real list where the BB of the hands will be extracted
     if len(contours) > 1:
         rect2 = cv2.minAreaRect(contours[1])
-        box2 = cv2.boxPoints(rect2)
-        box2 = np.int0(box2)
-        p2_1, p2_2 = getBB(box2)
+        lm, rm, tm, bm = getExtremeImg(frame_bgr, contours[1])
+        indices2 = [] # this is created to calculate the area of the first contour, which is the biggest area
+        indices2 = addPOintsToTheList(indices2, lm, rm, tm, bm)
+        p2_1, p2_2 = getBB(indices2)
         box2_area = (p2_2[0] - p2_1[0]) * (p2_2[1] - p2_1[1])
-        #print('cons')
         if box2_area > 1000:
-            #print('> 100')
-            indices = box1.tolist() + box2.tolist()
-            #print(type(box1))
-            #print('b1', box1, 'b2', box2, 'b3', indices)
-            #cv2.waitKey(0)
-        else:
-            indices = box1.tolist()   
-    else:
-        #print('else')
-        indices = box1.tolist()
-
-    p1_1, p1_2 = getBB(indices)
-        
-    
-    if box2_area >= 1000:
-        #print('contour 2, area: ', box2_area)
-        #cv2.rectangle(frame_resized, p2_1, p2_2, (0,255,0), 2)
-        my_key = True
-    cv2.rectangle(frame_resized, p1_1, p1_2, (255,0,0), 2)
+            indices = addPOintsToTheList(indices, lm, rm, tm, bm) # adding points to the real list where the BB of the hands will be extracted
+    if len(contours) >= 1 and len(indices) > 0:
+        p_main_1, p_main_2 = getBB(indices)
+        cv2.rectangle(frame_bgr, p_main_1, p_main_2, (0,0,255), 2) # kirmizi: iyi
 
     # Display the resulting frame
-    cv2.imshow('frame',frame_resized)
-    #if my_key:
-    #    cv2.waitKey(0)
+    cv2.imshow('frame',frame_bgr)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-    my_key = False
   
 # When everything done, release the capture
 cap.release()
